5ca3c4271fd9f4c21a5c3a5432f24dcbc3f2e407
buty4649 <buty4649@gmail.com>
2022-05-27 23:38:52 +0900

    Add debugger

--- reddish_parser/src/main.rs
+++ reddish_parser/src/main.rs
@@ -1,3 +1,31 @@
+use reddish_parser::parse_command_line;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    loop {
+        if input.is_empty() {
+            print!("$ ");
+        } else {
+            print!("> ")
+        }
+        io::stdout().flush().unwrap();
+
+        let mut buffer = String::new();
+        match io::stdin().read_line(&mut buffer) {
+            Err(_) => break,
+            Ok(n) if n == 0 => break,
+            Ok(_) => {
+                if buffer == "\n" {
+                    match parse_command_line(&input, 0, true) {
+                        Ok(_) => (),
+                        Err(e) => eprintln!("Error: {:?}", e),
+                    }
+                    input.clear();
+                } else {
+                    input.push_str(&buffer);
+                }
+            }
+        }
+    }
 }
